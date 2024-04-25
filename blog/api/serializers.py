8e1f93c224d3ca880c8e4a90bf51ca3cd9f289bf
Volodymyr Konoval <konoval.volodymyr@lll.kpi.ua>
2024-04-25 09:34:57 +0000

    Finish viewsets and routers

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -59,3 +59,8 @@ class TagField(serializers.SlugRelatedField):
         except (TypeError, ValueError):
           self.fail(f"Tag value {data} is invalid")
 
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = "__all__"
+
--- blog/api/urls.py
+++ blog/api/urls.py
@@ -2,16 +2,20 @@ from django.urls import path, include, re_path
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views
 
+from rest_framework.routers import DefaultRouter
+
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
 
 
-from blog.api.views import PostList, PostDetail, UserDetail
+#from blog.api.views import PostList, PostDetail, UserDetail, TagViewSet
+from blog.api.views import UserDetail, TagViewSet, PostViewSet
+
 
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
+    #path("posts/", PostList.as_view(), name="api_post_list"),
+    #path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
 
     path("auth/", include("rest_framework.urls"))
@@ -47,4 +51,14 @@ urlpatterns += [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+]
+
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
+
+urlpatterns += [
+    path("auth/", include("rest_framework.urls")),
+    # ... other patterns omitted
+    path("", include(router.urls)),
 ]
